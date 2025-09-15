b874a9cfa5af62154616e4f4a8a607232f304fdc
SMGhulam29091993 <smghulam.ghausfaiyaz19@gmail.com>
2025-09-15 19:34:53 +0530

    feat: added layout and auth folder structure for signup and signIn

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+interface AuthLayoutProps {
+  children: React.ReactNode;
+}
+
+const AuthLayout = ({ children }: AuthLayoutProps) => {
+  return <div>AuthLayout</div>;
+};
+
+export default AuthLayout;
