d5fa178041d18a64533ff8b0483bc759a82f1a3a
SMGhulam29091993 <smghulam.ghausfaiyaz19@gmail.com>
2025-09-15 23:20:32 +0530

    feat: added a nav with logo and name

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 interface RootLayoutProps {
@@ -5,7 +7,17 @@ interface RootLayoutProps {
 }
 
 const RootLayout = ({ children }: RootLayoutProps) => {
-  return <div>{children}</div>;
+  return (
+    <div className="root-layout">
+      <nav>
+        <Link href="/" className="flex items-center gap-2">
+          <Image src="/logo.svg" alt="Logo" height={32} width={38} />
+          <h2 className="text-primary-100">Interview with AI</h2>
+        </Link>
+      </nav>
+      {children}
+    </div>
+  );
 };
 
 export default RootLayout;
