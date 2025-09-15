e13a144588f23a38db5d2ebd1546b3da5c282139
SMGhulam29091993 <smghulam.ghausfaiyaz19@gmail.com>
2025-09-15 19:05:38 +0530

    feat: adding folder structure and types and public assets

---
+++ app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next";
+import { Mona_Sans } from "next/font/google";
+import "./globals.css";
+
+const monaSans = Mona_Sans({
+  variable: "--font-mona-sans",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Interview with AI",
+  description: "Prepare for your interviews with AI-powered mock interviews.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${monaSans.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
