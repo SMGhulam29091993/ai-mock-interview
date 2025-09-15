c942afc4957fd4538612746773684b68417d0cfd
SMGhulam29091993 <smghulam.ghausfaiyaz19@gmail.com>
2025-09-15 19:47:28 +0530

    feat: updated the layout and structure of main page.tsx inside root folder maintain a single layout after login

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+interface RootLayoutProps {
+  children?: React.ReactNode;
+}
+
+const RootLayout = ({ children }: RootLayoutProps) => {
+  return <div>RootLayout</div>;
+};
+
+export default RootLayout;
