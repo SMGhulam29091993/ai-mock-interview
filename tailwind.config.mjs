466587c69425f74946dea63b3f4bc12425ba14b0
SMGhulam29091993 <smghulam.ghausfaiyaz19@gmail.com>
2025-09-15 23:09:31 +0530

    feat: added next-themes, react-hook-form and sonner and eslint

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+const config = {
   darkMode: "class",
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
@@ -12,3 +12,5 @@ export default {
   },
   plugins: [],
 };
+
+export default config;
