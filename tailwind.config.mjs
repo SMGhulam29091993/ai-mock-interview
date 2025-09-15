3ec43557c7f068d842eb87af878b137bd7d2a776
SMGhulam29091993 <smghulam.ghausfaiyaz19@gmail.com>
2025-09-15 20:18:02 +0530

    feat: added tailwindcss postcss and autoprefixer

---
+++ tailwind.config.mjs
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: "class",
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./lib/**/*.{js,ts,jsx,tsx}",
+    "./constants/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
